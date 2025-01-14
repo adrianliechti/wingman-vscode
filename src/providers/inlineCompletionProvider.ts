ab58c2d0a9c2340334cd6a1c775bcac4dcebedab
Adrian Liechti <adrian@monobox.ch>
2025-01-14 22:48:13 +0100

    add a simple schema

--- src/providers/inlineCompletionProvider.ts
+++ src/providers/inlineCompletionProvider.ts
@@ -1,35 +1,142 @@
 import * as vscode from "vscode";
+import OpenAI from "openai";
+
+const client = new OpenAI({
+  apiKey: "-",
+  baseURL: "http://localhost:8080/v1",
+});
+
+type Message = {
+  name: string;
+  language: string;
+
+  content: string;
+  position: number;
+
+  prefix: string;
+  suffix: string;
+};
+
+let timer: NodeJS.Timeout | undefined;
+let cancelTokenSource: vscode.CancellationTokenSource | undefined;
 
 export const inlineCompletionItemProvider: vscode.InlineCompletionItemProvider =
-  {
-    async provideInlineCompletionItems(document, position, context, token) {
-      const message = {
-        name: document.fileName,
+{
+  provideInlineCompletionItems(document, position, context, token): vscode.ProviderResult<vscode.InlineCompletionItem[]> {
+    if (timer) {
+      clearTimeout(timer);
+      timer = undefined;
+    }
 
-        language: document.languageId,
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel();
+      cancelTokenSource.dispose();
+    }
 
-        content: document.getText(),
-        position: document.offsetAt(position),        
+    if (token.isCancellationRequested) {
+      return null;
+    }
 
-        prefix: textBefore(document, position),
-        suffix: textAfter(document, position),
-      };
+    if (document.uri.scheme === "vscode-scm") {
+      return null;
+    }
 
-      const result = await handleCompletionMessage(JSON.stringify(message));
+    const editor = vscode.window.activeTextEditor;
+
+    if (editor && editor.selections.length > 1) {
+      return null;
+    }
+
+    cancelTokenSource = new vscode.CancellationTokenSource();
+
+    const localToken = cancelTokenSource.token;
+
+    return new Promise((resolve, reject) => {
+      timer = setTimeout(async () => {
+        if (localToken.isCancellationRequested) {
+          return reject('Cancelled');
+        }
+
+        try {
+          const message = {
+            name: document.fileName,
       
-      return {
-        items: [
-          {
-            insertText: String(result),
-          },
-        ],
-      };
-    },
-  };
+            language: document.languageId,
+      
+            content: document.getText(),
+            position: document.offsetAt(position),
+      
+            prefix: textBefore(document, position),
+            suffix: textAfter(document, position),
+          };
 
-async function handleCompletionMessage(message: string): Promise<string> {
-    console.log(message);
-    return "hallo du";
+          const result = await handleCompletionMessage(message);
+
+          const completions = [
+            {
+              insertText: result,
+            },
+          ]
+
+          if (!localToken.isCancellationRequested) {
+            resolve(completions);
+          } else {
+            reject('Cancelled after fetch');
+          }
+        } catch (err) {
+          reject(err);
+        }
+      }, 2000); // 750ms debounce
+    });
+  },
+};
+
+async function handleCompletionMessage(message: Message): Promise<string> {
+  const system = "You are an expert in software development. Your Task is inline code completion in a code editor. Return the code snippet as JSON."
+  const content = "```" + message.language + "\n" + message.prefix + "`[SNIPPET]`" + message.suffix + "\n```"
+
+  const completion = await client.chat.completions.create({
+    model: 'gpt-4o-mini',
+
+    messages: [
+      { role: 'user', content: system + "\n\n" + content },
+    ],
+
+    response_format: {
+      type: 'json_schema',
+
+      json_schema: {
+        name: 'snippet',
+        description: 'the code snippet to be completed into the editor',
+
+        schema: {
+          type: 'object',
+
+          properties: {
+            // description: {
+            //   type: 'string',
+            //   description: 'textual description of the code snippet',
+            // },
+
+            code: {
+              type: 'string',
+              description: 'the code snippet without any markdown formatting',
+            },
+          },
+        },
+
+        //strict: true,
+      },
+    }
+  });
+
+  if (completion.choices.length === 0) {
+    return "";
+  }
+
+  const snippet = JSON.parse(completion.choices[0].message.content ?? "{}");
+
+  return snippet?.code ?? "";
 }
 
 function textBefore(document: vscode.TextDocument, position: vscode.Position) {
